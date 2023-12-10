bf1de8f4b5c27f8fdeec5b3dfb18dc9582120299
Alexander Amaya <105259049+amayaagro@users.noreply.github.com>
2023-12-09 19:55:25 -0500

    Actualización valores no negativos
    Para que al momento de introducir los valores no acepte valores negativos

--- secciones/Lote/editar.php
+++ secciones/Lote/editar.php
@@ -1,8 +1,8 @@
-<!-- Página Editar Lote -->
+<!-- Editar Lote -->
 
 <!-- SQL Editar Lote -->
 <?php
-include "../../bd.php";
+include("../../bd.php");
 
 $sentencia = $conexion->prepare("SELECT * FROM cultivo");
 $sentencia->execute();
@@ -56,35 +56,41 @@ if ($_POST) {
 ?>
 
 <!-- Header -->
-<?php include "../../templates/header.php"; ?>
+<?php include("../../templates/header.php"); ?>
 
+<!-- Herramienta Accesibilidad -->
+<script>
+    (function(d) {
+        var s = d.createElement("script");
+        s.setAttribute("data-account", "fFgZ6B1nWP");
+        s.setAttribute("src", "https://cdn.userway.org/widget.js");
+        s.setAttribute('locale', 'es');
+        (d.body || d.head).appendChild(s);
+    })(document)
+</script>
 <br />
 <!-- Formulario Editar Lote -->
 <div class="card">
     <div class="content">
         <div class="title">
-            <h2 id="Titulo"><strong>Modificar Datos Lote</strong><img src="../../Img/Logo.png" width="230" height="80"
-                    align="right"></h2>
+            <h2 id="Titulo"><strong>Modificar Datos Lote</strong><img src="../../Img/Logo.png" width="230" height="80" align="right"></h2>
         </div>
         <div class="card-body">
             <form action="" method="post" class="form" enctype="multipart/form-data">
                 <div class="mb-3">
                     <label for="txtId" class="form-label">Id</label>
-                    <input type="number" value="<?php echo $txtId; ?>" class="form-control" readonly name="txtId"
-                        id="txtId" aria-describedby="helpId" placeholder="ID">
+                    <input type="number" value="<?php echo $txtId; ?>" class="form-control" readonly name="txtId" id="txtId" aria-describedby="helpId" placeholder="ID">
                 </div>
                 <div class="mb-3">
                     <label for="nombrelote" class="form-label">Nombre Lote</label>
-                    <input type="text" value="<?php echo $nombredelote; ?>" class="form-control" name="nombrelote"
-                        id="nombrelote" aria-describedby="helpId" placeholder="Ingrese el nombre del lote">
+                    <input type="text" value="<?php echo $nombredelote; ?>" class="form-control" name="nombrelote" id="nombrelote" aria-describedby="helpId" placeholder="Ingrese el nombre del lote">
                 </div>
                 <div class="mb-3">
                     <label for="finca" class="form-label">Finca Asociada</label>
                     <select class="form-select form-select-sm" name="finca" id="finca">
                         <?php foreach ($fincas as $finca) { ?>
-                        <option <?php echo ($FincaAsociada == $finca['id']) ? "selected" : ""; ?>
-                            value="<?php echo $finca['id']; ?>">
-                            <?php echo $finca['Nombredelpredio']; ?></option>
+                            <option <?php echo ($FincaAsociada == $finca['id']) ? "selected" : ""; ?> value="<?php echo $finca['id']; ?>">
+                                <?php echo $finca['Nombredelpredio']; ?></option>
                         <?php } ?>
                     </select>
                 </div>
@@ -92,21 +98,18 @@ if ($_POST) {
                     <label for="cultivo" class="form-label">Cultivo</label>
                     <select class="form-select form-select-sm" name="cultivo" id="cultivo">
                         <?php foreach ($cultivos as $siembra) { ?>
-                        <option <?php echo ($Cultivo == $siembra['Id']) ? "selected" : ""; ?>
-                            value="<?php echo $siembra['Id']; ?>">
-                            <?php echo $siembra['Cultivo']; ?></option>
+                            <option <?php echo ($Cultivo == $siembra['Id']) ? "selected" : ""; ?> value="<?php echo $siembra['Id']; ?>">
+                                <?php echo $siembra['Cultivo']; ?></option>
                         <?php } ?>
                     </select>
                 </div>
                 <div class="mb-3">
                     <label for="tamaño" class="form-label">Tamaño (Ha)</label>
-                    <input type="number" value="<?php echo $Tamano; ?>" class="form-control" name="tamaño" id="tamaño"
-                        aria-describedby="helpId" placeholder="Ingrese el tamaño del lote">
+                    <input type="number" value="<?php echo $Tamano; ?>" class="form-control" name="tamaño" id="tamaño" min="0" aria-describedby="helpId" placeholder="Ingrese el tamaño del lote">
                 </div>
                 <div class="mb-3">
                     <label for="fechadesiembra" class="form-label">Fecha de Siembra</label>
-                    <input type="date" value="<?php echo $FechaDeSiembra; ?>" class="form-control" name="fechadesiembra"
-                        id="fechadesiembra" aria-describedby="helpId" placeholder="">
+                    <input type="date" value="<?php echo $FechaDeSiembra; ?>" class="form-control" name="fechadesiembra" id="fechadesiembra" aria-describedby="helpId" placeholder="">
                 </div>
                 </br>
                 <button type="submit" id="guardar" class="btn" title="Actualizar">Actualizar</button>
@@ -118,18 +121,7 @@ if ($_POST) {
 </div>
 
 <!-- Footer -->
-<?php include "../../templates/footer.php"; ?>
+<?php include("../../templates/footer.php"); ?>
 
 <!-- Social footer -->
-<?php include "../../templates/socfooter.php"; ?>
-
-<!-- Herramienta Accesibilidad -->
-<script>
-(function(d) {
-    var s = d.createElement("script");
-    s.setAttribute("data-account", "fFgZ6B1nWP");
-    s.setAttribute("src", "https://cdn.userway.org/widget.js");
-    s.setAttribute('locale', 'es');
-    (d.body || d.head).appendChild(s);
-})(document)
-</script>
+<?php include("../../templates/socfooter.php"); ?>
