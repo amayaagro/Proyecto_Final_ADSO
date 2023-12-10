fb17613dfb5df6087835caaee18dfd98f6fde13e
Alexander Amaya <105259049+amayaagro@users.noreply.github.com>
2023-12-09 20:09:25 -0500

    Actualización valores no negativos
    Para que al momento de introducir los valores no acepte valores negativos

--- secciones/Inventario/editar.php
+++ secciones/Inventario/editar.php
@@ -1,8 +1,8 @@
-<!-- Pägina Editar Insumo -->
+<!-- Editar Herramienta -->
 
-<!-- SQL Editar Insumo -->
+<!-- SQL Editar Herramienta -->
 <?php
-include "../../bd.php";
+include("../../bd.php");
 
 if ($_GET['txtID']) {
     $txtId = (isset($_GET['txtID']) ? $_GET['txtID'] : "");
@@ -55,59 +55,58 @@ if ($_POST) {
 ?>
 
 <!-- Header -->
-<?php include "../../templates/header.php"; ?>
+<?php include("../../templates/header.php"); ?>
 
+<!-- Herramienta Accesibilidad -->
+<script>
+    (function(d) {
+        var s = d.createElement("script");
+        s.setAttribute("data-account", "fFgZ6B1nWP");
+        s.setAttribute("src", "https://cdn.userway.org/widget.js");
+        s.setAttribute('locale', 'es');
+        (d.body || d.head).appendChild(s);
+    })(document)
+</script>
 <br />
 <!-- Formulario Editar Insumo -->
 <div class="card">
     <div class="content">
         <div class="title">
-            <h2 id="Titulo"><strong>Modificar Datos Insumo</strong><img src="../../Img/Logo.png" width="230"
-                    height="80" align="right"></h2>
+            <h2 id="Titulo"><strong>Modificar Datos Insumo</strong><img src="../../Img/Logo.png" width="230" height="80" align="right"></h2>
         </div>
         <div class="card-body">
             <form action="" method="post" class="form" enctype="multipart/form-data">
                 <div class="mb-3">
                     <label for="txtId" class="form-label">Id</label>
-                    <input type="number" value="<?php echo $txtId; ?>" class="form-control" readonly name="txtId"
-                        id="txtId" aria-describedby="helpId" placeholder="ID">
+                    <input type="number" value="<?php echo $txtId; ?>" class="form-control" readonly name="txtId" id="txtId" aria-describedby="helpId" placeholder="ID">
                 </div>
                 <div class="mb-3">
                     <label for="fechadeinventario" class="form-label">Fecha de Inventario</label>
-                    <input type="date" value="<?php echo $fechadeinventario; ?>" class="form-control"
-                        name="fechadeinventario" id="fechadeinventario" aria-describedby="helpId" placeholder="">
+                    <input type="date" value="<?php echo $fechadeinventario; ?>" class="form-control" name="fechadeinventario" id="fechadeinventario" aria-describedby="helpId" placeholder="">
                 </div>
                 <div class="mb-3">
                     <label for="nombreinsumo" class="form-label">Nombre del Insumo</label>
-                    <input type="text" value="<?php echo $nombreinsumo; ?>" class="form-control" name="nombreinsumo"
-                        id="nombreinsumo" aria-describedby="helpId" placeholder="Ingrese el nombre del insumo">
+                    <input type="text" value="<?php echo $nombreinsumo; ?>" class="form-control" name="nombreinsumo" id="nombreinsumo" aria-describedby="helpId" placeholder="Ingrese el nombre del insumo">
                 </div>
                 <div class="mb-3">
                     <label for="ingredienteactivo" class="form-label">Ingrediente Activo</label>
-                    <input type="text" value="<?php echo $ingredienteactivo; ?>" class="form-control"
-                        name="ingredienteactivo" id="ingredienteactivo" aria-describedby="helpId"
-                        placeholder="Ingrese el ingrediente activo">
+                    <input type="text" value="<?php echo $ingredienteactivo; ?>" class="form-control" name="ingredienteactivo" id="ingredienteactivo" aria-describedby="helpId" placeholder="Ingrese el ingrediente activo">
                 </div>
                 <div class="mb-3">
                     <label for="unidad" class="form-label">Unidad</label>
-                    <input type="text" v value="<?php echo $unidad; ?>" class="form-control" name="unidad" id="unidad"
-                        aria-describedby="helpId" placeholder="Ingrese la unidad">
+                    <input type="text" v value="<?php echo $unidad; ?>" class="form-control" name="unidad" id="unidad" aria-describedby="helpId" placeholder="Ingrese la unidad">
                 </div>
                 <div class="mb-3">
                     <label for="cantidadenbodega" class="form-label">Cantidad</label>
-                    <input type="number" value="<?php echo $cantidadenbodega; ?>" class="form-control"
-                        name="cantidadenbodega" id="cantidadenbodega" aria-describedby="helpId"
-                        placeholder="cantidad en bodega">
+                    <input type="number" value="<?php echo $cantidadenbodega; ?>" class="form-control" name="cantidadenbodega" id="cantidadenbodega" min="0" aria-describedby="helpId" placeholder="cantidad en bodega">
                 </div>
                 <div class="mb-3">
                     <label for="fechadecompra" class="form-label">Fecha de Compra</label>
-                    <input type="date" value="<?php echo $fechadecompra; ?>" class="form-control" name="fechadecompra"
-                        id="fechadecompra" aria-describedby="helpId" placeholder="">
+                    <input type="date" value="<?php echo $fechadecompra; ?>" class="form-control" name="fechadecompra" id="fechadecompra" aria-describedby="helpId" placeholder="">
                 </div>
                 <div class="mb-3">
                     <label for="fechadevencimiento" class="form-label">Fecha de Vencimiento</label>
-                    <input type="date" value="<?php echo $fechadevencimiento; ?>" class="form-control"
-                        name="fechadevencimiento" id="fechadevencimiento" aria-describedby="helpId" placeholder="">
+                    <input type="date" value="<?php echo $fechadevencimiento; ?>" class="form-control" name="fechadevencimiento" id="fechadevencimiento" aria-describedby="helpId" placeholder="">
                 </div>
                 </br>
                 <button type="submit" id="guardar" class="btn" title="Actualizar">Actualizar</button>
@@ -119,18 +118,7 @@ if ($_POST) {
 </div>
 
 <!-- Footer -->
-<?php include "../../templates/footer.php"; ?>
+<?php include("../../templates/footer.php"); ?>
 
 <!-- Social footer -->
-<?php include "../../templates/socfooter.php"; ?>
-
-<!-- Herramienta Accesibilidad -->
-<script>
-(function(d) {
-    var s = d.createElement("script");
-    s.setAttribute("data-account", "fFgZ6B1nWP");
-    s.setAttribute("src", "https://cdn.userway.org/widget.js");
-    s.setAttribute('locale', 'es');
-    (d.body || d.head).appendChild(s);
-})(document)
-</script>
+<?php include("../../templates/socfooter.php"); ?>
