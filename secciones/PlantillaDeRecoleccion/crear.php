a7efbef08b994a01c6165d633e7835c6d5102257
Alexander Amaya <105259049+amayaagro@users.noreply.github.com>
2023-12-09 20:15:12 -0500

    Actualización valores no negativos
    Para que al momento de introducir los valores no acepte valores negativos

--- secciones/PlantillaDeRecoleccion/crear.php
+++ secciones/PlantillaDeRecoleccion/crear.php
@@ -1,7 +1,8 @@
-<!-- Página Crear Recolección -->
+<!-- Crear Recolección -->
+
 <!-- SQL Crear Recolección -->
 <?php
-include "../../bd.php";
+include("../../bd.php");
 
 $sentencia = $conexion->prepare("SELECT * FROM trabajadores WHERE Estado = 1");
 $sentencia->execute();
@@ -35,56 +36,58 @@ if ($_POST) {
 ?>
 
 <!-- Header -->
-<?php include "../../templates/header.php"; ?>
+<?php include("../../templates/header.php"); ?>
 
+<!-- Herramienta Accesibilidad -->
+<script>
+    (function(d) {
+        var s = d.createElement("script");
+        s.setAttribute("data-account", "fFgZ6B1nWP");
+        s.setAttribute("src", "https://cdn.userway.org/widget.js");
+        s.setAttribute('locale', 'es');
+        (d.body || d.head).appendChild(s);
+    })(document)
+</script>
 <br />
 <!-- Formulario Crear Recoleccción -->
 <div class="card">
     <div class="content">
         <div class="title">
-            <h id="Titulo"><strong>Registrar Recolección</strong><img src="../../Img/Logo.png" width="230" height="80"
-                    align="right"></h>
+            <h id="Titulo"><strong>Registrar Recolección</strong><img src="../../Img/Logo.png" width="230" height="80" align="right"></h>
         </div>
         <div class="card-body">
             <form action="" method="post" class="form" enctype="multipart/form-data">
                 <div class="mb-3">
                     <label for="fechaderecoleccion" class="form-label">Fecha de Recolección</label>
-                    <input type="date" class="form-control" name="fechaderecoleccion" id="fechaderecoleccion"
-                        aria-describedby="helpId" placeholder="">
+                    <input type="date" class="form-control" name="fechaderecoleccion" id="fechaderecoleccion" aria-describedby="helpId" placeholder="">
                 </div>
                 <div class="mb-3">
                     <label for="responsable" class="form-label">Responsable</label>
                     <select class="form-select form-select-sm" name="responsable" id="responsable">
                         <?php foreach ($empleados as $empleado) { ?>
-                        <option value="<?php echo $empleado['Id']; ?>"><?php echo $empleado['Nombre']; ?></option>
+                            <option value="<?php echo $empleado['Id']; ?>"><?php echo $empleado['Nombre']; ?></option>
                         <?php } ?>
                     </select>
                 </div>
                 <div class="mb-3">
                     <label for="kilosrecolectados" class="form-label">Kilos Recolectados</label>
-                    <input type="number" class="form-control" name="kilosrecolectados" id="kilosrecolectados" value=""
-                        required aria-describedby="helpId" placeholder="Kilos Recolectados">
+                    <input type="number" class="form-control" name="kilosrecolectados" id="kilosrecolectados" value="" min="0" required aria-describedby="helpId" placeholder="Kilos Recolectados">
                 </div>
                 <div class="mb-3">
                     <label for="jornalesderecoleccion" class="form-label">Jornales Empleados</label>
-                    <input type="number" class="form-control" name="jornalesderecoleccion" id="jornalesderecoleccion"
-                        value="" required aria-describedby="helpId"
-                        placeholder="Ingrese los jornales utilizados en la recolección">
+                    <input type="number" class="form-control" name="jornalesderecoleccion" id="jornalesderecoleccion" value="" min="0" required aria-describedby="helpId" placeholder="Ingrese los jornales utilizados en la recolección">
                 </div>
                 <div class="mb-3">
                     <label for="valorneto" class="form-label">Valor Neto</label>
-                    <input type="number" onChange="CalcularTotal()" class="form-control" name="valorneto" id="valorneto"
-                        value="" required aria-describedby="helpId" placeholder="Ingrese el valor neto">
+                    <input type="number" onChange="CalcularTotal()" class="form-control" name="valorneto" id="valorneto" value="" min="0" required aria-describedby="helpId" placeholder="Ingrese el valor neto">
                 </div>
                 <div class="mb-3">
                     <label for="descuentos" class="form-label">Descuento</label>
-                    <input type="number" onChange="CalcularTotal()" class="form-control" name="descuentos"
-                        id="descuentos" value="" required aria-describedby="helpId" placeholder="Ingrese el descuento">
+                    <input type="number" onChange="CalcularTotal()" class="form-control" name="descuentos" id="descuentos" value="" min="0" required aria-describedby="helpId" placeholder="Ingrese el descuento">
                 </div>
                 <div class="mb-3">
                     <label for="total" class="form-label">Valor a Pagar</label>
-                    <input type="number" class="form-control" name="total" id="total" aria-describedby="helpId"
-                        placeholder="Ingrese el valor total">
+                    <input type="number" class="form-control" name="total" id="total" min="0" aria-describedby="helpId" placeholder="Ingrese el valor total">
                 </div>
                 </br>
                 <button type="submit" id="guardar" class="btn" title="Agregar">Agregar</button>
@@ -97,31 +100,20 @@ if ($_POST) {
 
 <!-- Función Cálculo Resta -->
 <script>
-function CalcularTotal() {
+    function CalcularTotal() {
 
-    var descuentos = $("#descuentos").val();
-    var valorneto = $("#valorneto").val();
+        var descuentos = $("#descuentos").val();
+        var valorneto = $("#valorneto").val();
 
-    if (descuentos >= 0 && valorneto >= 0) {
-        $("#total").val(valorneto - descuentos);
+        if (descuentos >= 0 && valorneto >= 0) {
+            $("#total").val(valorneto - descuentos);
+        }
+
     }
-
-}
 </script>
 
 <!-- Footer -->
-<?php include "../../templates/footer.php"; ?>
+<?php include("../../templates/footer.php"); ?>
 
 <!-- Social Footer -->
-<?php include "../../templates/socfooter.php"; ?>
-
-<!-- Herramienta Accesibilidad -->
-<script>
-(function(d) {
-    var s = d.createElement("script");
-    s.setAttribute("data-account", "fFgZ6B1nWP");
-    s.setAttribute("src", "https://cdn.userway.org/widget.js");
-    s.setAttribute('locale', 'es');
-    (d.body || d.head).appendChild(s);
-})(document)
-</script>
+<?php include("../../templates/socfooter.php"); ?>
